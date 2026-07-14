80930a4e1db656cd011852281dd7c4bf4208f8c9
mildredmwandizi34-cell <mildredmwandizi34@gmail.com>
2026-07-14 11:30:18 +0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,41 @@
+// Load shipments
+let shipments = JSON.parse(localStorage.getItem("shipments")) || [];
+
+// Save shipments
+function saveShipments() {
+    localStorage.setItem("shipments", JSON.stringify(shipments));
+}
+
+// Display shipments
+function loadShipments() {
+    let table = document.getElementById("shipmentTable");
+
+    if (!table) return;
+
+    table.innerHTML = "";
+
+    shipments.forEach((s, index) => {
+        table.innerHTML += `
+        <tr>
+            <td>${s.tracking}</td>
+            <td>${s.sender}</td>
+            <td>${s.receiver}</td>
+            <td>${s.status}</td>
+            <td>
+                <button onclick="deleteShipment(${index})">
+                    Delete
+                </button>
+            </td>
+        </tr>
+        `;
+    });
+}
+
+// Delete shipment
+function deleteShipment(index){
+    shipments.splice(index,1);
+    saveShipments();
+    loadShipments();
+}
+
+window.onload = loadShipments;
