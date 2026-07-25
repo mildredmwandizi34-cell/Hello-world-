abb5485b7213bb6d6f85fd014b79b9f935fdcbfa
mildredmwandizi34-cell <mildredmwandizi34@gmail.com>
2026-07-26 00:28:02 +0300

    Update create-shipment.js

--- create-shipment.js
+++ create-shipment.js
@@ -7,19 +7,50 @@ document.getElementById("shipmentForm").addEventListener("submit", function(e){
     let tracking = "AGL" + Math.floor(100000 + Math.random() * 900000);
 
     let shipment = {
+
+        // Basic Information
         tracking: tracking,
         sender: document.getElementById("sender").value,
         receiver: document.getElementById("receiver").value,
         package: document.getElementById("package").value,
         weight: document.getElementById("weight").value + " kg",
-        status: "Shipment Created"
+
+        // Shipping Details
+        service: "Air Freight",
+
+        origin: "",
+        destination: "",
+
+        route: "",
+
+        // Shipment Status
+        status: "Shipment Created",
+        progress: 5,
+
+        // Tracking
+        location: "American Global Logistics Warehouse",
+
+        vehicle: "Waiting for Dispatch",
+
+        delivery: "Pending",
+
+        // Shipment History
+        history:
+            "📦 Shipment Created<br>" +
+            "📍 American Global Logistics Warehouse<br>" +
+            "🕒 " + new Date().toLocaleString()
+
     };
 
     shipments.push(shipment);
 
     localStorage.setItem("shipments", JSON.stringify(shipments));
 
-    alert("Shipment Created Successfully!\nTracking Number: " + tracking);
+    alert(
+        "Shipment Created Successfully!\n\nTracking Number: " +
+        tracking
+    );
 
     this.reset();
+
 });
